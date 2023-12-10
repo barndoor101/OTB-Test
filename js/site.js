b4720727bf6d7efe3c1ac4752461f07ac668e4a4
Andrew Berryman <andrew.berryman@gmail.com>
2023-12-10 00:36:01 +0000

    start of js

--- js/site.js
+++ js/site.js
@@ -1,3 +1,21 @@
-import Handlebars from "handlebars";
-const template = Handlebars.compile("Name: {{name}}");
-console.log(template({ name: "Nils" }));
+//import Handlebars from "handlebars";
+
+async function getResults() {
+    let url = 'data/hotels.json';
+    try {
+        let res = await fetch(url, {
+            mode: "no-cors",
+            credentials: "same-origin",
+            eaders: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+              }
+        });
+        return await res.json();
+        console.log(res);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getResults();
