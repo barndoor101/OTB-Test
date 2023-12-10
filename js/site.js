d30843284a3a98b05fbc277e1d86ff38fdcb4a89
Andrew Berryman <andrew.berryman@gmail.com>
2023-12-10 03:20:03 +0000

    json working

--- js/site.js
+++ js/site.js
@@ -1,21 +1,14 @@
 //import Handlebars from "handlebars";
 
-async function getResults() {
-    let url = 'data/hotels.json';
-    try {
-        let res = await fetch(url, {
-            mode: "no-cors",
-            credentials: "same-origin",
-            eaders: {
-                "Content-Type": "application/json",
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-              }
-        });
-        return await res.json();
-        console.log(res);
-    } catch (error) {
-        console.log(error);
-    }
+async function getData(){
+    fetch('./data/hotels.json')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
 }
 
-getResults();
+getData();
+
+function renderResults() {
+    
+}
