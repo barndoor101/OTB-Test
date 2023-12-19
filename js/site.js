6bfd268551f97c16142484f8ce93e5a8ea4efead
Andrew Berryman <andrew.berryman@gmail.com>
2023-12-19 08:26:23 +0000

    mostly working

--- js/site.js
+++ js/site.js
@@ -1,14 +1,53 @@
-//import Handlebars from "handlebars";
+var rawData;
+var sortOption = "price";
 
 async function getData(){
     fetch('./data/hotels.json')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            rawData = data;
+            renderResults(rawData, sortOption);
+        })
         .catch(error => console.log(error));
 }
 
-getData();
+function renderResults(data, sortOption) {
 
-function renderResults(sortOption) {
-    console.log(sortOption);
+    let holidayData = data.data.holidayResults;
+    let hotelData = data.data.hotels;
+    let resultArray = new Array;
+
+    holidayData.forEach(holiday => {
+        hotelData.forEach(hotel => {
+            if (holiday.hotelId === hotel.id) {
+                holidayResult = {...holiday, ...hotel}
+                resultArray.push(holidayResult)
+            }
+            
+        });
+    });
+
+    var holidayResult = document.getElementById('holidayResult').innerHTML;
+    var compiled_template = Handlebars.compile(holidayResult);
+    var rendered = compiled_template(resultArray);
+    document.getElementById('contentContainer').innerHTML = rendered;
 }
+
+function expandDrawer(id) {
+    let drawer = $(id).siblings('.expandDrawer')
+    console.log(drawer);
+    drawer.toggleClass('hide');
+}
+
+window.onload = function() {
+    getData();
+
+    Handlebars.registerHelper('renderStars', (rating) => {
+        let result = '';
+        for (let i = 1; i <= rating; i++) {
+          result += `&#9733;`;
+        }
+        return new Handlebars.SafeString(result);
+    });
+
+};
